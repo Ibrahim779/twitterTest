d8b8ec4697c752ee6c87b4a1e31a5fefe526fa6a
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-09 18:17:21 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('Site')->middleware('auth')->group(function () {
+    Route::get('/', 'PostController@index')->name('posts.index');
+    Route::resource('posts', 'PostController')->only(['store', 'show']);
+    Route::post('comments/posts/{post}', 'CommentController@store')->name('comments.store');
+    Route::get('loves/{status}/posts/{post}', 'LoveController@store')->name('loves.store');
+    Route::get('follows/users/{user}', 'FollowController@store')->name('follows.store');
+    Route::resource('users', 'UserController')->only(['index','show','update']);
+    Route::get('users/{user}/followers', 'UserController@followers')->name('users.followers');
 });
+
+Auth::routes();
